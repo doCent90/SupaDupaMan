b9407f5008aac1461d088511ed43c2ab97d0fc8e
doCent90 <78255120+doCent90@users.noreply.github.com>
2021-12-15 17:32:22 +0300

    Add files via upload

---
+++ Assets/Scripts/Camera/CameraShaker.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using Cinemachine;
+
+public class CameraShaker : MonoBehaviour
+{
+    [SerializeField] private LasersActivator _lasersActivator;
+
+    private CinemachineVirtualCamera _virtualCamera;
+
+    private const int On = 1;
+    private const int Off = 0;
+
+    private void OnEnable()
+    {
+        _virtualCamera = GetComponent<CinemachineVirtualCamera>();
+        _lasersActivator.Fired += TryShake;
+    }
+
+    private void OnDisable()
+    {
+        _lasersActivator.Fired -= TryShake;
+    }
+
+    private void TryShake(bool isFire)
+    {
+        if (isFire)
+            _virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = On;
+        else
+            _virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = Off;
+    }
+}
